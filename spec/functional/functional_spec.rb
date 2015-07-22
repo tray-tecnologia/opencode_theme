2e20db4cdb46c0843a691b9402ba58498f495fa8
Samuel Lourenço <slourenco@tray.net.br>
2015-07-22 09:41:50 -0300

    adicionando casos de teste para 'list' e 'bootstrap'

--- spec/functional/functional_spec.rb
+++ spec/functional/functional_spec.rb
@@ -7,16 +7,24 @@ describe OpencodeTheme::Cli, :functional do
   PASSWORD = '14ae838d9e971465af45b35803b8f0a4'
   THEME_ID = ''
   
-  before do
-    
+  after(:all) do
+    # clearing generated and downloaded files
+    FileUtils.cd '..'
+    FileUtils.rm 'config.yml'
+    FileUtils.rm_rf 'default'
   end
   
-  after(:context) do
-    FileUtils.rm 'config.yml'
+  context 'Invalid or Inexistent Configuration' do
+    it 'does not list when the config is invalid' do
+      pending 'redmine issue 37588'
+      output = capture(:stdout) { subject.list }
+      expect(output).to include '[FAIL]'
+    end
   end
-    
+  
   context 'Configure' do
     it 'fails to create config.yml file when called with no arguments' do
+      pending 'redmine issue 37590'
       output = capture(:stdout) { subject.configure }
       expect(output).to include '[FAIL]'
     end
@@ -24,7 +32,22 @@ describe OpencodeTheme::Cli, :functional do
     it 'creates config.yml when called with parameters' do
       output = capture(:stdout) { subject.configure API_KEY, PASSWORD }
       expect(output).to include 'Configuration [OK]'
-      # => check for yml file here
+      expect(File.exists? 'config.yml').to eq true
+    end
+  end
+  
+  context 'Bootstrap' do
+    after(:context) do
+      Dir.chdir '..'
+    end
+    
+    it 'create new theme' do
+      output = capture(:stdout) { subject.bootstrap API_KEY, PASSWORD }
+      expect(output).to include 'Configuration [OK]'
+      expect(output).to include 'Create default theme on store'
+      expect(output).to include 'Saving configuration to default'
+      expect(output).to include 'Downloading default assets from Opencode'
+      expect(output).to include 'Done.'
     end
   end
   
@@ -32,16 +55,21 @@ describe OpencodeTheme::Cli, :functional do
     let(:output) { capture(:stdout) { subject.list } }
     
     it 'lists all the themes from the store' do
-      expect(output).to be_nil
+      output = capture(:stdout) { subject.list }
+      expect(output).to include 'Theme name:'
+      expect(output).to include 'Theme ID:'
+      expect(output).to include 'Theme status:'
     end
   end
     
   context 'Publishing Theme' do
-    let(:output) { capture(:stdout) { subject.publish } }
-    
     it 'publishes theme' do
+      pending 'redmine issue 36815'
+      Dir.chdir 'default'
+      output = capture(:stdout) { subject.publish }
       expect(output).not_to include '[FAIL]'
       expect(output).to include '[SUCCESS]'
+      Dir.chdir '..'
     end
   end
   
@@ -49,6 +77,7 @@ describe OpencodeTheme::Cli, :functional do
     let(:output) { capture(:stdout) { subject.systeminfo } }
     
     it 'displays system information' do
+      pending 'redmine issue 37576'
       expect(output).not_to be_nil
     end
   end
